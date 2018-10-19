930f11b78adf3b50e6162cfcae0fd7abbbcb912b
Muhanned Abdulhaq Mohammed Noman <muhanned.noman@gmail.com>
2018-10-19 12:22:25 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function theTruth(){
+  return true;
+}
+function sayHey(){
+  return 'Hey!';
+}
+function sayHeyFriend(name){
+  return `Hey, ${name}!`;
+}
+function favoriteIceCream(iceCream){
+  return `I love ${iceCream}`;
+}
+function shouting(shout){
+  return shout.toUpperCase();
+}
+function roundDown(number){
+  return Math.floor(number);
+}
